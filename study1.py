ef774f113da72eb4951615ec79c198738ba3d931
leo <leo.yuan.ext@nokia.com>
2017-09-12 05:09:09 -0400

    study note 1

---
+++ study1.py
@@ -0,0 +1,15 @@
+
+line = '======================='
+box = '''
+--------------
+|            |
+--------------
+'''
+
+print('I think', 150+5, 'dollar is ok')
+print (box)
+name = input('pls input your name:')
+age = input('pls input your age:')
+print ()
+print('Welcome back to Nokia,',name,age,'!')
+print (line)
