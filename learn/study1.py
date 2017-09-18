b9e6f563477e2336b9a5c741c7ff08f19f6f84ec
leo <leo.yuan.ext@nokia.com>
2017-09-18 04:11:57 -0400

    study note

--- learn/study1.py
+++ learn/study1.py
@@ -13,7 +13,19 @@ print('I think' 'pl', 150+5, 'dollar is ok')
 print ()
 name = input('pls input your name:')
 age = input('pls input your age:')
-#use "" to include ' as special symbol. and use \ to set " as string.
+
+#int(str) transfer string to number
+agenum = int(age)
+if agenum >=18:
+    print('_adult_')
+if agenum <=12:
+    print ('_child_')
+else:
+    print ('_teenager_')
+
+
+#use "" to include ' as special symbol, or use '' to include "
+# and use \ to set " as string.
 print("Welcome back to \"Nokia\", I'm",name,'of',age,'!')
 print (line)
 
