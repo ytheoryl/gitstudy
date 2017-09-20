e8ee8b8cbb86dd9c328196a31407b7d0429cac71
leo <leo.yuan.ext@nokia.com>
2017-09-20 03:35:34 -0400

    study

--- learn/FINANCE.py
+++ learn/FINANCE.py
@@ -8,13 +8,36 @@ ZHENRONGBAO30 = 0.065
 ZHENRONGBAO90 = 0.075
 ZHENRONGBAO365 = 0.085
 
-total = input('pls inpur your total money')
+moneyratioid = {'id1': 'YUEBAO', 'id1-1': 0.04,
+              'id2': 'WANGSHANG', 'id2-1': 0.0415,
+              'id3': 'ZHENRONGBAO', 'id3-1': 0.0554
+}
 
+income = 0
 
-yearly = i * total
+#moneyratio = [YUEBAO, WANGSHANG, ZHENRONGBAO, ZHENRONGBAO30, ZHENRONGBAO90, ZHENRONGBAO365]
+moneyratio = [YUEBAO, WANGSHANG, ZHENRONGBAO]
+moneyrationame = ('YUEBAO', 'WANGSHANG', 'ZHENGRONGBAO')
+
+for i in moneyratio:
+    print(i)
+
+total = float(input('pls inpur your total money:'))
+
+yearly = total
 quaterly = yearly / 4
 monthly = yearly / 12
-daily =  yearly / 365
+daily = yearly / 365
+timeframe = [yearly, quaterly, monthly, daily]
+timeframeid = ('yearly', 'quaterly')
 
-for i in valuelist:
-    i * x
+for n in timeframe:
+    for x in timeframeid:
+        print('===%s===' % timeframeid[0])
+    for i in moneyratio:
+        income = n * i
+    for y in moneyrationame:
+        print('%s %.2f' % (y, income))
+
+
+
