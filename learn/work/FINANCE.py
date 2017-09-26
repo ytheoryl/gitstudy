f6c608928e48dcd7163659dea261752ca4bc1d37
leo <leo.yuan.ext@nokia.com>
2017-09-26 04:41:33 -0400

    study note

--- learn/work/FINANCE.py
+++ learn/work/FINANCE.py
@@ -1,45 +1,29 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-YUEBAO = 0.04
-WANGSHANG = 0.0415
-ZHENRONGBAO = 0.0554
-ZHENRONGBAO30 = 0.065
-ZHENRONGBAO90 = 0.075
-ZHENRONGBAO365 = 0.085
+
+moneyratiodict = {'YUEBAO': 0.04, 'WANGSHANG': 0.0415, 'ZHEN': 0.0554,
+                  'ZHEN30': 0.065, 'ZHEN90': 0.075, 'ZHEN365': 0.085}
+
+for x, y in moneyratiodict.items():
+    print('%s年化收益是%s%%' % (x, y*100))
+
+total = float(input('your total investment:'))
 
 
-
-moneyratiodict = {'id1': 'YUEBAO', 'id1-1': 0.04,
-              'id2': 'WANGSHANG', 'id2-1': 0.0415,
-              'id3': 'ZHENRONGBAO', 'id3-1': 0.0554
-}
-
-income = 0
-
-#moneyratio = [YUEBAO, WANGSHANG, ZHENRONGBAO, ZHENRONGBAO30, ZHENRONGBAO90, ZHENRONGBAO365]
-moneyratio = [YUEBAO, WANGSHANG, ZHENRONGBAO]
-moneyrationame = ('YUEBAO', 'WANGSHANG', 'ZHENGRONGBAO')
-
-for i in moneyratio:
-    print(i)
-
-total = float(input('pls inpur your total money:'))
-
-yearly = total
-quaterly = yearly / 4
-monthly = yearly / 12
-daily = yearly / 365
-timeframe = [yearly, quaterly, monthly, daily]
-timeframeid = ('yearly', 'quaterly')
-
-for n in timeframe:
-    for x in timeframeid:
-        print('===%s===' % timeframeid[0])
-    for i in moneyratio:
-        income = n * i
-    for y in moneyrationame:
-        print('%s %.2f' % (y, income))
+def allin(u, i):
+    income = total * i
+    incomem = income / 12
+    incomed = income / 365
+    print('The income of %9s| \t%.2f\t%.2f\t%.2f' % (u, income, incomem, incomed))
+    print('------------------------------------------------')
 
 
+print('''
+------------------------------------------------
+                            YEAR    MONTH   DAY
+================================================
+''')
 
+for x, y in moneyratiodict.items():
+    allin(x, y)
