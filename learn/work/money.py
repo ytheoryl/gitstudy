b7b0d007177edcd690f637cdfb19e56805236fde
leo <leo.yuan.ext@nokia.com>
2017-10-11 22:08:58 -0400

    new

---
+++ learn/work/money.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+moneyratiodict = {'YUEBAO': 0.04, 'WANGSHANG': 0.0415, 'ZHEN': 0.0554,
+                  'ZHEN30': 0.065, 'ZHEN90': 0.075, 'ZHEN365': 0.085}
+
+for x, y in moneyratiodict.items():
+    print('%s今日的年化收益是%s%%' % (x, y*100))
+
+total = float(input('your total investment:'))
+
+
+def allin(u, i):
+    income = total * i
+    incomem = income / 12
+    incomed = income / 365
+    print('The income of %9s| \t%.2f\t%.2f\t%.2f' % (u, income, incomem, incomed))
+    print('------------------------------------------------')
+
+
+print('''
+------------------------------------------------
+                            YEAR    MONTH   DAY
+================================================
+''')
+
+for x, y in moneyratiodict.items():
+    allin(x, y)
